e82b00f82f60b18bc64c9962d469ba5fd57a5000
gcontiero11 <gcontiero011@gmail.com>
2025-05-21 09:36:04 -0300

    refactor: changing task_menu name to task_menu_context

--- app/src/main/java/com/dev/contiero/personaltasks/activity/MainActivity.kt
+++ app/src/main/java/com/dev/contiero/personaltasks/activity/MainActivity.kt
@@ -51,7 +51,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.task_menu,menu)
+        menuInflater.inflate(R.menu.task_menu_context,menu)
         return true
     }
 }
