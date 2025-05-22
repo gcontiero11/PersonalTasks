11f010623eee2cec43f3441fd15b0033c53a4e8f
gcontiero11 <gustcontiero@gmail.com>
2025-05-22 13:24:46 -0300

    feat: adding editFields with DatePickerDialog and TimePickerDialog at the activity_create_task.xml and adding a new attribute called dateTime at Task.kt

--- app/src/main/java/com/dev/contiero/personaltasks/model/Task.kt
+++ app/src/main/java/com/dev/contiero/personaltasks/model/Task.kt
@@ -2,7 +2,12 @@ package com.dev.contiero.personaltasks.model
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
+import java.time.LocalDateTime
 
 @Parcelize
-data class Task(val id: Int, val title: String, val description: String) :Parcelable
-//data class Task(val id: Int, val title: String, val description: String)
+data class Task(
+    val id: Int,
+    val title: String,
+    val description: String,
+    val dateTime: LocalDateTime,
+) :Parcelable
